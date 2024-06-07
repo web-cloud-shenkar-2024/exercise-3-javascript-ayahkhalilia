24062e4fc099eae2f639ed9042029c2c85cfd0a9
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-06-07 22:20:55 +0300

    first func

---
+++ exercise-3-javascript-ayahkhalilia-master/js/scripts.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    fetch("/exercise-3-javascript-ayahkhalilia/data/music.json")
+          .then(Response =>Response.json())
+          .then(data => console.log(data));
+}
+
+function initRectangles() {
+}
+
+function initSongs() {
+}
+
+function chooseRectangleColor() {
+}
+
+function addRectangle() {
+}
+
+function subtractRectangle() {
+}
+
+function switchRectanglesSongs() {
+}
+
+function populateSongsInList() {
+}
