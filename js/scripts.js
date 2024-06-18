2ec88f4298b957a1977318e127a79c1b7a551e02
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-06-18 12:48:27 +0300

    fixed the add func and put the base vars needed

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
+var array_name= ["A","y","a","h","K","h","a","l","i","l","i","a"];
+var curr_arr_count=0;
+var colors = ["#98FB98", "#63C5DA", "blue", "#33B864"];
+var curr_color_count=0;
+let save=" ";
+var rectangles_count=0;
+var rectangle=0;
 window.onload = () => {
-    fetch("/data/music.json")
-          .then(Response =>Response.json())
-          .then(data => console.log(data));
+    for (let i = 0; i< 12; i++){
+        addRectangle();
+     }
 }
 
 function initRectangles() {
@@ -14,12 +21,16 @@ function chooseRectangleColor() {
 }
 
 function addRectangle() {
-    var black_rectangle=document.getElementById("black_rectangle");
+    const black_rectangle=document.getElementById("black_rectangle");
     var new_rectangle=document.createElement("div");
     new_rectangle.className="small_rectangles";
+
     new_rectangle.style.backgroundColor=chooseRectangleColor();
     new_rectangle.innerHTML=initRectangles();
+        rectangles_count++;
     black_rectangle.appendChild(new_rectangle);
+
+    
 }
 
 function subtractRectangle() {
